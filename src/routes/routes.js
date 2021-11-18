09767559025a3d662327c589df3f42c68f887f5f
Vinícius Rossi <viniciusprossi18@gmail.com>
2021-11-18 19:51:41 -0300

    remoção da verificação de jwt para métodos de listagens

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,20 +7,20 @@ const events = require("../events");
 const routes = Router();
 
 //Products
-routes.get("/products/:page/:maxProducts", login.verifyJWT, products.getProducts);
+routes.get("/products/:page/:maxProducts", products.getProducts);
 routes.post("/products", login.verifyJWT, products.createProduct);
 routes.put("/products", login.verifyJWT, products.updateProduct);
 routes.delete("/products", login.verifyJWT, products.deleteProduct);
 
 //Payment
-routes.get("/payment", login.verifyJWT, payment.makePayment);
+routes.post("/payment", login.verifyJWT, payment.makePayment);
 
 //Users
 routes.post("/login", login.login);
 routes.post("/createUser", login.createUser);
 
 //Events
-routes.get("/events/:page/:maxresults", login.verifyJWT, events.getEvents);
+routes.get("/events/:page/:maxresults", events.getEvents);
 routes.post("/events", login.verifyJWT, events.createEvent);
 routes.put("/events", login.verifyJWT, events.updateEvent);
 routes.delete("/events", login.verifyJWT, events.deleteEvent);
